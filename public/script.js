d4fcb1f7d218154473b7c08ba96174c8890c0e1b
Collin Trammell <collin.trammell@gmail.com>
2017-07-15 19:21:00 -0500

    First commit

---
+++ public/script.js
@@ -0,0 +1,4 @@
+var myModule = angular.module('myModule', []);
+myModule.controller('MsgCtrl', function($scope,$http) {
+     $scope.test = 'This is my test';      
+});
---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+// Create our express app
+var app = express();
+
+// Mount are website (inside the public directory) at /
+app.use('/', express.static('./public'));
+
+// Listen on port 3000
+app.listen(3000, function(err) {
+	console.log('Listening at http://localhost:3000');
+});
